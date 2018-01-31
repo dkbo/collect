7fe06d7db7ad2ff958e05f151a27e7666953f885
label <label@landteam.net>
2018-01-31 10:27:18 +0800

    add

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,39 +1,40 @@
 import { createStore, applyMiddleware, combineReducers, compose } from 'redux'
-import { routerReducer, routerMiddleware } from 'react-router-redux'
-import createHistory from 'history/createHashHistory'
+import { routerReducer as routing, routerMiddleware } from 'react-router-redux'
 import { createEpicMiddleware } from 'redux-observable'
-import promise from 'redux-promise'
+import promise from 'redux-promise';
 // import logger from 'redux-logger';
 import * as reducers from '../reducers'
 import rootEpics from '../epics'
-const rootReducer = combineReducers({
-	...reducers,
-	router: routerReducer
-})
-export const history = createHistory()
-const routeMiddleware = routerMiddleware(history)
+
 const epicMiddleware = createEpicMiddleware(rootEpics)
+// const epicMiddleware = createEpicMiddleware(epics.searchGithubKeywordEpics)
+const createStoreWithMiddleware = compose(
+  applyMiddleware(promise, routerMiddleware(history)),
+)(createStore);
+const rootReducer = combineReducers({
+  ...reducers,
+  routing,
+});
 
-const middleware = [
-	rootReducer,
-	routeMiddleware
-]
-// if (process.env.NODE_ENV !== 'production') {
-// 	middleware.push(window.devToolsExtension && window.devToolsExtension())
-// }
-
-export const configureStore = initialState => {
-	const store = createStore(
-		...middleware,
-		compose(applyMiddleware(promise, epicMiddleware), window.devToolsExtension && window.devToolsExtension()),
-		initialState
-	)
-	if (module.hot) {
-		// Enable Webpack hot module replacement for reducers
-		module.hot.accept('../reducers', () => {
-			store.replaceReducer(reducers)
-		})
-	}
-
-	return store
+const middleware = [rootReducer]
+if (process.env.NODE_ENV !== 'production') {
+  middleware.push(window.devToolsExtension && window.devToolsExtension())
 }
+const configureStore = (initialState) => {
+  const store = createStoreWithMiddleware(
+    ...middleware,
+    applyMiddleware(epicMiddleware),
+    initialState,
+  );
+
+  if (module.hot) {
+    // Enable Webpack hot module replacement for reducers
+    module.hot.accept('../reducers', () => {
+      store.replaceReducer(reducers);
+    });
+  }
+
+  return store;
+}
+
+export default configureStore
