6a87f566b5b418cf2d446a1c4700476eae95dd2f
label <label@landteam.net>
2018-01-31 10:27:00 +0800

    add

--- src/index.jsx
+++ src/index.jsx
@@ -1,22 +1,15 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
-import createHistory from 'history/createHashHistory';
 import { Router } from 'react-router'
-import { Route, Switch } from 'react-router-dom';
-import { ConnectedRouter, routerReducer, routerMiddleware } from 'react-router-redux';
-
-import currentHistory from './config/currentHistory'
-
-import configureStore from './store/configureStore'
+import { Route, Switch } from 'react-router-dom'
+import { ConnectedRouter } from 'react-router-redux'
+import { configureStore, history } from './store/configureStore'
 import routes from './router'
-
 import { chat, geo } from './config/firebase'
-
 // import './index.sass'
-const history = createHistory();
 
-const store = configureStore();
+const store = configureStore()
 
 firebase.initializeApp(chat)
 firebase.initializeApp(geo, 'geo')
@@ -26,12 +19,11 @@ firebase.chatAH = firebase.auth()
 
 const geoApp = firebase.app('geo')
 firebase.geoDB = firebase.database(geoApp)
-
-
+console.log(store)
 render(
-  <Provider store={store}>
-    <ConnectedRouter history={history} routes={routes} />
-    {/* <Router history={history} routes={routes} /> */}
-  </Provider>,
-    document.getElementById('app'),
+	<Provider store={store}>
+		<ConnectedRouter history={history} routes={routes} />
+		{/* <Router history={history} routes={routes} /> */}
+	</Provider>,
+	document.getElementById('app')
 )
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,40 +1,39 @@
 import { createStore, applyMiddleware, combineReducers, compose } from 'redux'
-import { routerReducer as routing, routerMiddleware } from 'react-router-redux'
+import { routerReducer, routerMiddleware } from 'react-router-redux'
+import createHistory from 'history/createHashHistory'
 import { createEpicMiddleware } from 'redux-observable'
-import promise from 'redux-promise';
+import promise from 'redux-promise'
 // import logger from 'redux-logger';
 import * as reducers from '../reducers'
 import rootEpics from '../epics'
-
-const epicMiddleware = createEpicMiddleware(rootEpics)
-// const epicMiddleware = createEpicMiddleware(epics.searchGithubKeywordEpics)
-const createStoreWithMiddleware = compose(
-  applyMiddleware(promise, routerMiddleware(history)),
-)(createStore);
 const rootReducer = combineReducers({
-  ...reducers,
-  routing,
-});
+	...reducers,
+	router: routerReducer
+})
+export const history = createHistory()
+const routeMiddleware = routerMiddleware(history)
+const epicMiddleware = createEpicMiddleware(rootEpics)
 
-const middleware = [rootReducer]
-if (process.env.NODE_ENV !== 'production') {
-  middleware.push(window.devToolsExtension && window.devToolsExtension())
+const middleware = [
+	rootReducer,
+	routeMiddleware
+]
+// if (process.env.NODE_ENV !== 'production') {
+// 	middleware.push(window.devToolsExtension && window.devToolsExtension())
+// }
+
+export const configureStore = initialState => {
+	const store = createStore(
+		...middleware,
+		compose(applyMiddleware(promise, epicMiddleware), window.devToolsExtension && window.devToolsExtension()),
+		initialState
+	)
+	if (module.hot) {
+		// Enable Webpack hot module replacement for reducers
+		module.hot.accept('../reducers', () => {
+			store.replaceReducer(reducers)
+		})
+	}
+
+	return store
 }
-const configureStore = (initialState) => {
-  const store = createStoreWithMiddleware(
-    ...middleware,
-    applyMiddleware(epicMiddleware),
-    initialState,
-  );
-
-  if (module.hot) {
-    // Enable Webpack hot module replacement for reducers
-    module.hot.accept('../reducers', () => {
-      store.replaceReducer(reducers);
-    });
-  }
-
-  return store;
-}
-
-export default configureStore
